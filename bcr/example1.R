1e03d26291cac50a9d5e979ac2510954e82f3c27
Harish S. Bhat <hbhat@ucmerced.edu>
2018-02-06 12:37:52 -0800

    Adding Bayesian credible regions code from Bondell's web site

---
+++ bcr/example1.R
@@ -0,0 +1,24 @@
+rm(list=ls(all=TRUE))
+source('BayesPen.R')
+
+require(mvtnorm)
+rho = 0.9
+sigma = 1
+n = 60
+p = 50
+times = 1:p
+H = abs(outer(times, times, "-"))
+V = sigma * rho^H
+set.seed(77)
+
+beta = rep(0,p)
+beta[11:15] = runif(5)
+beta[36:40] = runif(5)
+x = rmvnorm(n,rep(0,p),V)
+y = x%*%beta + rnorm(n)
+
+# Fit the model
+prior = list(varE=list(df=3,S=1),varBR=list(df=3,S=1))
+example_fit = Bayes.pen(y, x, prior=prior)
+example_fit$order.joint
+example_fit$order.marg
